83fbe3f35bad294898e3f63eaeb94de9ecd2eaf7
Antony Martinez Ruiz <antonymaruiz02@gmail.com>
2024-06-05 06:35:51 -0500

    Add Spring Boot and it's dependecies

---
+++ src/main/java/com/riwi/class_online/ClassOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.class_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/class_online/ClassOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.class_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
