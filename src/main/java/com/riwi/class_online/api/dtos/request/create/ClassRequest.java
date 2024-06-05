62c660e1d7e0f2b67bacb2ecf70bb3aa1b83bbc9
Antony Martinez Ruiz <antonymaruiz02@gmail.com>
2024-06-05 08:28:52 -0500

    Update Validation in DTO's

--- src/main/java/com/riwi/class_online/api/dtos/request/create/ClassRequest.java
+++ src/main/java/com/riwi/class_online/api/dtos/request/create/ClassRequest.java
@@ -1,5 +1,8 @@
 package com.riwi.class_online.api.dtos.request.create;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +15,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClassRequest {
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Description is required")
     private String description;
+
+    @NotNull(message = "Active is required")
+    @Pattern(regexp = "true|false")
     private boolean active;
 }
--- src/main/java/com/riwi/class_online/api/dtos/request/create/LessonRequest.java
+++ src/main/java/com/riwi/class_online/api/dtos/request/create/LessonRequest.java
@@ -1,5 +1,8 @@
 package com.riwi.class_online.api.dtos.request.create;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,8 +15,16 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LessonRequest {
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Content is required")
     private String content;
+
+    @NotNull(message = "Active is required")
+    @Pattern(regexp = "true|false")
     private boolean active;
+
+    @NotNull(message = "Class ID is required")
     private Long classId;
 }
